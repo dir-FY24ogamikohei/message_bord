f4b68bdb3f0af3c710823c1e87f41f65cb2f42f7
dir-FY24ogamikohei <kohei.ogami.h858@dirbato.co.jp>
2024-04-23 14:36:33 +0900

    destroy

--- message_bord/src/contollers/DestroyServlet.java
+++ message_bord/src/contollers/DestroyServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.message;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class DestroyServlet
@@ -26,7 +28,25 @@ public class DestroyServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	    String _token = request.getParameter("_token");
+        if (_token != null && _token.equals(request.getSession().getId())) {
+            var em = DBUtil.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            var m = em.find(message.class, (Integer) (request.getSession().getAttribute("message_id")));
+
+            em.getTransaction().begin();
+            em.remove(m); // データ削除
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("message_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
 	}
 
 }
